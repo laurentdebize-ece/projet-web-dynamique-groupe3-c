4f0480baadb9348c31e00d2d6c30d90eea1c37b1
Charles Masson <charles.masson@edu.ece.fr>
2023-05-10 21:30:25 +0200

    ajout première fonctions avec la bdd

---
+++ fonction.php
@@ -0,0 +1,61 @@
+<?php
+
+function select($PDO,$table,$where=null) {
+    $sql = "SELECT * FROM $table";
+    if ($where != null) {
+        $sql .= " WHERE $where";
+    }
+    /*echo $sql;*/
+    $stmt = $PDO->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+?>
+
+
+<?php
+
+function selectcible($PDO,$table,$where=null,$cibles) {
+    $sql = "SELECT $cibles FROM $table";
+    if ($where != null) {
+        $sql .= " WHERE $where";
+    }
+    $stmt = $PDO->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+?>
+
+<?php
+
+$colonne = array('ID_Matiere','Nom','Volume_horaire');
+$string = implode(",",$colonne);
+function selectcible2($PDO,$table,$string,$where=null) {
+    
+    $sql = "SELECT $string FROM $table";
+    if ($where != null) {
+        $sql .= " WHERE $where";
+    }
+    echo $sql;
+
+}
+
+?>
+
+<?php
+
+function selection($PDO,$table,$where=null,$cibles=null,$cibles2=null,$cibles3=null,$cibles4=null,$cible5) {
+    $sql = "SELECT * FROM $table";
+    if ($where != null) {
+        $sql .= " WHERE $where";
+    }
+    /*echo $sql;*/
+    $stmt = $PDO->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+?>
