48cf1f6c3a7d51349ed5015495682caa0b5438e8
noemieruat <noemie.ruat@edu.ece.fr>
2023-05-11 13:53:04 +0200

    ajout eleve

--- BDD/Omnes.sql
+++ BDD/Omnes.sql
@@ -102,30 +102,4 @@ CREATE TABLE  `compte/competence` (
 
 
 INSERT INTO `ecole` (`Nom`) VALUES ('Ecole 42');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-INSERT INTO (`ecole`) VALUES ('GUGUGAGASCHOOL');
+INSERT INTO `compte` (`Nom`, `Prenom`, `E_mail`, `MDP`, `Type_compte`) VALUES (`Gogo`, `Gadget`, `gg@gmail.com`, `gg`, `etudiant`);
