5f2fb79fc63ca537d64a8de9a4b1c6383de75e26
noemieruat <noemie.ruat@edu.ece.fr>
2023-05-15 10:10:32 +0200

    correction nom bdd

--- homePage/homePage.php
+++ homePage/homePage.php
@@ -31,6 +31,9 @@ if (!isset($_SESSION['ID_Compte']) && !isset($_SESSION['Type_compte'])) {
             <div class="flexboxText-menu"><a href="../mesCompetencesPage/mesCompetencesPage.php" class="lienWhite">Mes compétences</a></div>
             <div class="flexboxText-menu"><a href="../competencesTransversesPage/competencesTransversesPage.html" class="lienWhite">Compétences transverses</a></div>
             <div class="flexboxText-menu"><a href="../toutesCompetencesPage/toutesCompetencesPage.php" class="lienWhite">Toutes les compétences</a></div>
+            <?php //if($Type_compte=="admin"){ ?>
+                <div class="flexboxText-menu"><a href="../comptes/comptes.php" class="lienWhite">Comptes</a></div>
+            <?php //} ?>
             <div class="flexboxLogo-menu"><a href="../profilPage/profilPage.php" class="lienWhite"><img src="../img/profilLogo.png" class="menuLogo" alt=" profilLogo "></a></div>
         </div>
     </section>
