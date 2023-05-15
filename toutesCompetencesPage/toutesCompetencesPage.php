5e6610c1e0b6ee8bb502f50d05877a8bb8ec48e2
noemieruat <noemie.ruat@edu.ece.fr>
2023-05-15 13:22:50 +0200

    correction compte admin

--- toutesCompetencesPage/toutesCompetencesPage.php
+++ toutesCompetencesPage/toutesCompetencesPage.php
@@ -43,7 +43,7 @@ $_SESSION['Type_compte'] = $Type_compte;
             <div class="flexboxText-menu"><a href="../mesCompetencesPage/mesCompetencesPage.php" class="lienWhite">Mes compétences</a></div>
             <div class="flexboxText-menu"><a href="../competencesTransversesPage/competencesTransversesPage.html" class="lienWhite">Compétences transverses</a></div>
             <div class="flexboxText-menu"><a href="toutesCompetencesPage.php" class="lienClique">Toutes les compétences</a></div>
-            <?php if($Type_compte=="admin"){ ?>
+            <?php if($Type_compte=="Administrateur"){ ?>
                 <div class="flexboxText-menu"><a href="../comptesPage/comptesPage.php" class="lienWhite">Comptes</a></div>
             <?php } ?>
             <div class="flexboxLogo-menu"><a href="../profilPage/profilPage.php" class="lienWhite"><img src="../img/profilLogo.png" class="menuLogo" alt=" profilLogo "></a></div>
