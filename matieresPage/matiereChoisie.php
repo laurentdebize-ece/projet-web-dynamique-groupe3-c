1203bba9dce2ff96db7398162adb80c4166ef186
emmabatherosse <emma.batherosse@edu.ece.fr>
2023-05-18 22:13:37 +0200

    tableaux compétences

--- matieresPage/matiereChoisie.php
+++ matieresPage/matiereChoisie.php
@@ -25,31 +25,32 @@ require_once('../fonction.php');
 $Nom_Matiere_Choisie = $_POST['Matiere'];
 $_SESSION['Nom_Matiere_Choisie']=$Nom_Matiere_Choisie;
 ?>
-<?php 
-if($Type_compte=="Etudiant"){?>
-            <form method="POST" action="../autoevaluation.php" id="AutoEval">
-            <?php echo "faire une auto-evaluation" ?>
-            <input type="submit" name ="faireEval" value="s'auto evaluer" class="boutonAutoEval">
-            </form>
-            <?php }
+<div class="login-form3">
+    <?php 
+    if($Type_compte=="Etudiant"){?>
+                <form method="POST" action="../autoevaluation.php" id="AutoEval">
+                <?php echo "faire une auto-evaluation" ?>
+                <input type="submit" name ="faireEval" value="s'auto evaluer" class="boutonAutoEval">
+                </form>
+                <?php }
 
-if($Type_compte=="admin"){
-    $reponseCompetence = $bdd->query("SELECT * FROM matiere
-        INNER JOIN matiere_competence ON matiere.ID_matiere = matiere_competence.ID_matiere
-        INNER JOIN competence ON matiere_competence.ID_competence = competence.ID_competence");
-}
+    if($Type_compte=="admin"){
+        $reponseCompetence = $bdd->query("SELECT * FROM matiere
+            INNER JOIN matiere_competence ON matiere.ID_matiere = matiere_competence.ID_matiere
+            INNER JOIN competence ON matiere_competence.ID_competence = competence.ID_competence");
+    }
 
-else {
-    $reponseCompetence = $bdd->query("SELECT DISTINCT * FROM compte 
-    INNER JOIN compte_competence ON compte.ID_Compte = compte_competence.ID_Compte
-    INNER JOIN competence ON compte_competence.ID_Competence = competence.ID_Competence
-    INNER JOIN matiere_competence ON competence.ID_Competence = matiere_competence.ID_Competence
-    INNER JOIN matiere ON matiere_competence.ID_Matiere = matiere.ID_Matiere
-    WHERE compte.ID_Compte = '$ID'
-    AND matiere.Nom_Matiere = '$Nom_Matiere_Choisie'");
-}
-?>
-
+    else {
+        $reponseCompetence = $bdd->query("SELECT DISTINCT * FROM compte 
+        INNER JOIN compte_competence ON compte.ID_Compte = compte_competence.ID_Compte
+        INNER JOIN competence ON compte_competence.ID_Competence = competence.ID_Competence
+        INNER JOIN matiere_competence ON competence.ID_Competence = matiere_competence.ID_Competence
+        INNER JOIN matiere ON matiere_competence.ID_Matiere = matiere.ID_Matiere
+        WHERE compte.ID_Compte = '$ID'
+        AND matiere.Nom_Matiere = '$Nom_Matiere_Choisie'");
+    }
+    ?>
+</div>
 
 <!DOCTYPE html>
 <html>
@@ -85,27 +86,30 @@ else {
         </div>
     </section>
     <section id="competencesParMatiere">
-    <table>
-    <tr id="textLigne1">
-        <td>Compétences</td>
-        <td>Matière</td>
-        <td>Etat de la compétence</td>
-    </tr>
-<?php while ($donneesCompetence = $reponseCompetence->fetch()){ ?> 
-    <tr>
-        <td id="textColonne1"><?php echo $donneesCompetence['Nom_competence']?></td>
-        <td id="textColonne"><?php echo $donneesCompetence['ID_Competence']?></td>
-        <td id="textColonne"><?php echo $donneesCompetence['Nom_matiere']?></td>
-        <td class="textColonne"><?php echo $donneesCompetence['Etat_competence']?></td>
-    </tr>
-<?php } ?>
-</table>
-<?php if($Type_compte=="Administrateur"){?>
-            <form method="POST" action="modifMatiere.php"  id="formModifMatiere">
-                <input type="submit" name ="modifMatiere" value="Supprimer" class="boutonModif">
-                <input type="submit" name ="modifMatiere" value="Modifier" class="boutonModif">
-            </form>
-        <?php }?>
+    <div class="login-form3">
+        <table>
+        <tr id="textLigne1">
+            <th>Compétences</th>
+            <th>Compétences</th>
+            <th>Matière</th>
+            <th>Etat de la compétence</th>
+        </tr>
+    <?php while ($donneesCompetence = $reponseCompetence->fetch()){ ?> 
+        <tr>
+            <td id="textColonne1"><?php echo $donneesCompetence['Nom_competence']?></td>
+            <td id="textColonne"><?php echo $donneesCompetence['ID_Competence']?></td>
+            <td id="textColonne"><?php echo $donneesCompetence['Nom_matiere']?></td>
+            <td class="textColonne"><?php echo $donneesCompetence['Etat_competence']?></td>
+        </tr>
+    <?php } ?>
+    </table>
+    <?php if($Type_compte=="Administrateur"){?>
+                <form method="POST" action="modifMatiere.php"  id="formModifMatiere">
+                    <input type="submit" name ="modifMatiere" value="Supprimer" class="boutonModif">
+                    <input type="submit" name ="modifMatiere" value="Modifier" class="boutonModif">
+                </form>
+            <?php }?>
+    </div>        
         </section>
     
     <footer>
