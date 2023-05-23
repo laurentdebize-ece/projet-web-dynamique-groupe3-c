a193c2b0e9a4195d512bb9644b497c372926fa57
LucasBoj <lucasboj39@gmail.com>
2023-05-23 18:18:32 +0200

    modifier competence

--- toutesCompetencesPage/modifCompetence.php
+++ toutesCompetencesPage/modifCompetence.php
@@ -45,7 +45,7 @@ $reponseModifCompetence = $_POST['modifCompetence'];?>
             <div class="flexboxText-menu"><a href="../matieresPage/matieresPage.php" class="lienWhite">Matières</a></div>
             <?php if($Type_compte=="Etudiant"){ ?>
                 <div class="flexboxText-menu"><a href="../mesCompetencesPage/mesCompetencesPage.php" class="lienWhite">Mes compétences</a></div>
-            <?php }
+            <?php } ?>
             <?php if($Type_compte=="Administrateur" || $Type_compte=="Etudiant"){ ?>
                 <div class="flexboxText-menu"><a href="../competencesTransversesPage/competencesTransversesPage.php" class="lienWhite">Compétences transverses</a></div>
                 <div class="flexboxText-menu"><a href="toutesCompetencesPage.php" class="lienClique">Toutes les compétences</a></div>
